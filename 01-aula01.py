453d45c23f651a564e61e9b19e5bece6eebcbec8
luizzzxt <luiznetotinho@gmail.com>
2024-09-11 15:11:16 -0300

    Add files via upload

---
+++ 01-aula01.py
@@ -0,0 +1,22 @@
+# Aqui eu posso adicionar comentários
+# Continuar na linha de baixo
+print("Luiz") # comentário a linha do código
+print('Luiz')
+print('Luiz "neto" Carlos')
+print("Luiz 'neto' carlos")
+print("Luiz \"Neto\" Carlos")
+
+teste= "oi"
+print(teste)
+print(type(teste))
+teste = 17
+print(teste)
+print(type(teste))
+teste = 12.1
+print(teste)
+print(type(teste))
+teste = False
+print (teste)
+print(type(teste))
+
+
