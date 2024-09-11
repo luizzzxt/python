599f2f141755dc8cbe4b1c3b1d5a9cb0eb63aba9
luizzzxt <luiznetotinho@gmail.com>
2024-09-11 15:22:09 -0300

    Add files via upload

---
+++ 02-aula02.py
@@ -0,0 +1,9 @@
+# atribuição das variáveis
+nome = "luiz"
+idade = 16
+altura = 1.75
+
+# exibir mensagem na tela
+print("nome: ", nome, " idade: ", idade, " altura: ", altura)
+# formação com f-string
+print(f"nome: {nome} idade: {idade} altura: {altura}")
